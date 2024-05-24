3fcbee736bc80a2ddbd74344c1ad4cd8f9b6e844
prabeenbhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-05-24 22:50:15 +0545

    first

---
+++ src/components/Skills.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import {motion} from "framer-motion"
+
+
+
+const Skill = ({name, x, y})=>{
+    return (
+        <motion.div className='flex items-center justify-center rounded-full font-semibold
+        bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:text-dark dark:bg-light
+        lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent
+        xs:text-dark xs:dark:text-light xs:font-bold'
+        whileHover={{scale:1.05}}
+        initial={{x:0, y:0}}
+        animate = {{x:x, y:y}}
+        transition={{duration: 1.5}}
+        viewport={{once:true}}>
+          {name}
+       </motion.div>
+
+    )
+}
+const Skills = () => {
+  return (
+    <>
+    <h2 className='font-bold text-6xl mt-60 w-full text-center  md:text-6xl xs:text-4xl md:mb-16'>Skills</h2>
+    <div className='w-full h-screen relative flex items-center justify-center rounded-full
+     bg-circularLight dark:bg-circularDark
+     lg:h-[80vh] sm:h-[60vh] xs:h-[50vh] 
+     lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+     
+     md:bg-circularLightLg md:dark:bg-circularDarkLg
+     sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+     '>
+        <motion.div className='flex items-center justify-center rounded-full font-semibold
+         bg-dark text-light p-8 shadow-dark cursor-pointer dark:bg-light dark:text-dark
+         lg:p-6 md:p-4 xs:text-xs xs:p-2'
+         whileHover={{scale:1.05}}>
+            Web
+        </motion.div>
+
+<Skill name="CSS" x="-20vw" y="2vw"/>
+<Skill name="HTML5" x="-5vw" y="-10vw"/>
+<Skill name="JavaScript" x="20vw" y="6vw"/>
+<Skill name="ReactJS" x="0vw" y="28vw"/>
+<Skill name="Flutter" x="-20vw" y="-15vw"/>
+<Skill name="AWS Cloud" x="15vw" y="-24vw"/>
+<Skill name="WordPress" x="-25vw" y="-28vw"/>
+<Skill name="Laravel" x="0vw" y="-40vw"/>
+<Skill name="SEO & Security" x="0vw" y="18vw"/>
+<Skill name="SQL & Firebase" x="30vw" y="-14vw"/>
+<Skill name="Next Js" x="-35vw" y="-10vw"/>
+<Skill name="OOP & Design" x="25vw" y="20vw"/>
+<Skill name="App Dev." x="35vw" y="10vw"/>
+<Skill name="Software Dev." x="-35vw" y="10vw"/>
+<Skill name="UI/UX Design" x="-25vw" y="20vw"/>
+<Skill name="Linux" x="-3vw" y="40vw"/>
+
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default Skills
---
+++ src/pages/index.js
@@ -0,0 +1,62 @@
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import Profile from "../../public/images/profile/file.png";
+import Image from 'next/image'
+import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
+import { LinkArrow } from '@/components/Icons';
+import HireMe from '@/components/HireMe';
+import Laptop from "../../public/images/profile/laptop.svg";
+import TransitionEffect from '@/components/TransitionEffect';
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Portfolio : Prabeen Bhattarai</title>
+        <meta name="description" content="Prabeen Bhattarai : Full Stack Developer Netx js, WordPress, PHP, Flutter and more.." />
+      
+      </Head>
+      <TransitionEffect />
+      <main className='flex items-center text0dark w-full min-h-screen dark:text-light'>
+   
+   <Layout className='pt-0 md:pt-16 sm:pt-8'>
+  <div className='flex items-center justify-between w-full lg:flex-col'>
+    <div className='w-1/2 md:w-full '>
+      <Image src={Profile} alt="Prabeen Bhattarai" className='w-full h-auto lg:hidden md:inline-block md:w-full'/>
+
+     
+    </div>
+    <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+      
+      <AnimatedText text="Bringing Creative Visions to Life with Code and Design." className='!text-left
+      xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl'/>
+      <p className='my-4 text-base font-medium md:text-sm sm:text-xs'> 
+      As a passionate programmer, I specialize in turning innovative visions into reality through seamless code and thoughtful design. With a keen eye for detail and a commitment to excellence, I transform complex ideas into engaging and functional digital experiences.</p>
+      <div className='flex items-center self-start mt-2 lg:self-center'>
+      <Link href="/dummy.pdf" target={"_blank"}
+      className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
+      hover:bg-light hover:text-dark border-2 
+      border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
+      md:p-2 md:px-4 md:text-base' download={true}>Resume 
+      <LinkArrow className={"w-6 ml-1"}/></Link>  
+      <Link href="mailto:prabeenbhattarai7450@gmail.com?cc=info@bhattarai-prabin.com.np&subject=Regarding Project Discussion [From Site:Bhattarai-Prabin.com.np]" target={"_blank"}
+      className='ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base'>Contact</Link>
+
+      </div>
+    </div>
+    
+  </div>
+    </Layout>
+    <HireMe/>
+    <div className='absolute left-20 top-20 inline-block w-40 md:hidden dark:hidden '>
+      <Image src={Laptop} alt="prabeen" className='w-full h-auto'/>
+    </div>
+   
+    
+      </main>
+    </>
+  )
+}
